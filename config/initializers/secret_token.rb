0e036135edd0868b989a04af75fd24960aa95975
Mary Wong <mspwong@hotmail.com>
2015-09-03 00:16:31 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sand::Application.config.secret_token = '0e268bdf8337ea83fbc9c9ef1145814c3dea2291ded52e9b53bf70c57e55c2f93e2ecf7f397cbe094111f2bb85b8a3b5b8f66891df6c68eaf670a793e5216227'
